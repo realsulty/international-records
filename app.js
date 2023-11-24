0376a6cb1c43bd6d5621cd9514e68714ed3d6ae9
realsulty <Sultanobm1@gmail.com>
2023-11-24 16:12:36 +0000

    added sections

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+// this functions displays the menu and hide the push button
+
+// function menu() {
+//     document.getElementById('menu-items').style.display = "block";
+//     document.getElementById('btn').style.display = "none";
+
+// }
+
+// This function allow you show and hide with same button
 function menu() {
-    document.getElementById('menu-items').style.display = "block";
-    document.getElementById('btn').style.display = "none";
-}
+    let mobileBtn = document.getElementById("menu-items");
+    if (mobileBtn.style.display === "block") {
+      mobileBtn.style.display = "none";
+    } else {
+      mobileBtn.style.display = "block";
+    }
+  }
