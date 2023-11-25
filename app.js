984947d787a77a4167c1c59b6872a3c5ea7404fb
realsulty <Sultanobm1@gmail.com>
2023-11-25 23:35:47 +0000

    fixed bugs

--- app.js
+++ app.js
@@ -9,9 +9,9 @@
 // This function allow you show and hide with same button
 function menu() {
     let mobileBtn = document.getElementById("menu-items");
-    if (mobileBtn.style.display === "block") {
+    if (mobileBtn.style.display === "flex") {
       mobileBtn.style.display = "none";
     } else {
-      mobileBtn.style.display = "block";
+      mobileBtn.style.display = "flex";
     }
   }
