a442bcf8d89b30cc1fac0887065e53aa39cfd01a
realsulty <Sultanobm1@gmail.com>
2023-11-27 21:36:11 +0000

    added styles

--- app.js
+++ app.js
@@ -22,9 +22,20 @@ function menu() {
   x = typeof myString;
   x = myString.length;
 //   x = myString[0].toUpperCase();
-//   z = typeof myString;
+//   z = typeof myString; a bit longer way
 //   z = myString.length
 //   z = myString.substring(1,9);
 x = myString.replace('d','D') // much eaiser and faster way of solving 
 
-console.log(x);
+
+
+console.log(x); // insert print Z if used the longer method 
+
+// other solution 1:
+// myNewstring = myString.charAt(0).toUpperCase() + myString.substring(1); and then cosole log it 
+// All of the following are math oprerators and methods you can use 
+// when you need to round numbers to the closest decimle or generate a random number
+
+
+// Math.round Math.floor 
+// x = Math.floor(Math.random() * 10 + 1); (this one will make number basic with no decimls))
